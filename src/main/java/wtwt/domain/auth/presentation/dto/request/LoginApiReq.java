bb07044aecd906df3ef42c8570adcb10e4279a57
uijin-j <uijin0531@gmail.com>
2024-05-09 22:57:54 +0900

    feat: Controller DTO명에 Api 추가

--- src/main/java/wtwt/domain/auth/presentation/dto/request/LoginApiReq.java
+++ src/main/java/wtwt/domain/auth/presentation/dto/request/LoginApiReq.java
@@ -1,6 +1,7 @@
 package wtwt.domain.auth.presentation.dto.request;
 
 import jakarta.validation.constraints.NotBlank;
+import wtwt.domain.auth.application.dto.requset.LoginReq;
 
 public record LoginApiReq(
     @NotBlank
@@ -8,5 +9,12 @@ public record LoginApiReq(
     @NotBlank
     String password
 ) {
+
+    public LoginReq toLoginReq() {
+        return LoginReq.builder()
+            .email(email)
+            .password(password)
+            .build();
+    }
 
 }
--- src/main/java/wtwt/domain/auth/presentation/dto/response/LoginApiRes.java
+++ src/main/java/wtwt/domain/auth/presentation/dto/response/LoginApiRes.java
@@ -1,9 +1,21 @@
 package wtwt.domain.auth.presentation.dto.response;
 
+import lombok.Builder;
+import wtwt.domain.auth.application.dto.response.TokensAndUserSummaryRes;
+
+@Builder
 public record LoginApiRes(
     String accessToken,
     String refreshToken,
     UserSummaryApiRes user
 ) {
+
+    public static LoginApiRes from(TokensAndUserSummaryRes tokensAndUserSummaryRes) {
+        return LoginApiRes.builder()
+            .accessToken(tokensAndUserSummaryRes.tokens().accessToken())
+            .refreshToken(tokensAndUserSummaryRes.tokens().refreshToken())
+            .user(UserSummaryApiRes.from(tokensAndUserSummaryRes.user()))
+            .build();
+    }
 
 }
