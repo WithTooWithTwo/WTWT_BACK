bfc64e5961f423576ef60b5dba7bf61c87759572
uijin-j <uijin0531@gmail.com>
2024-06-17 02:30:22 +0900

    doc: LoginApiReq API 세부 명세 추가

--- src/main/java/wtwt/domain/auth/presentation/dto/request/LoginApiReq.java
+++ src/main/java/wtwt/domain/auth/presentation/dto/request/LoginApiReq.java
@@ -1,12 +1,16 @@
 package wtwt.domain.auth.presentation.dto.request;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import wtwt.domain.auth.application.dto.requset.LoginReq;
 
+@Schema(description = "로그인 요청")
 public record LoginApiReq(
     @NotBlank
+    @Schema(description = "이메일", example = "you_me@naver.com")
     String email,
     @NotBlank
+    @Schema(description = "비밀번호", example = "you_me125!")
     String password
 ) {
 
